c3ae8f7c40389dc0751d5046d2d93916fc982b17
Rey David <reydavid391764@gmail.com>
2016-11-24 17:49:18 -0600

    Segunda parte By Rey

--- Ping-POO/src/todo/Juego.java
+++ Ping-POO/src/todo/Juego.java
@@ -8,6 +8,7 @@ public class Juego {
         //Atri
         JFrame ventana;
         JPanel panelJuego;
+        JLabel mesa;
         JLabel pelota;
         JLabel raquetaI;
         JLabel raquetaD;
@@ -29,19 +30,33 @@ public class Juego {
             panelJuego.setVisible(true);
             ventana.add(panelJuego);
             
+            mesa= new JLabel();
+            mesa.setSize(panelJuego.getSize());
+            mesa.setLocation(0, 0);
+            mesa.setIcon(new ImageIcon("imagenes/Tablero.png"));
+            mesa.setVisible(true);
+            panelJuego.add(mesa, 0);
+            
             raquetaI= new JLabel();
             raquetaI.setSize(10, 100);
             raquetaI.setLocation(0, 180);
             raquetaI.setIcon(new ImageIcon("imagenes/Raqueta 1.png"));
             raquetaI.setVisible(true);
-            panelJuego.add(raquetaI);
+            panelJuego.add(raquetaI, 0);
             
             raquetaD= new JLabel();
             raquetaD.setSize(10, 100);
             raquetaD.setLocation(985, 180);
             raquetaD.setIcon(new ImageIcon("imagenes/Raqueta 2.png"));
             raquetaD.setVisible(true);
-            panelJuego.add(raquetaD);
+            panelJuego.add(raquetaD, 0);
+            
+            pelota= new JLabel();
+            pelota.setSize(25, 25);
+            pelota.setLocation(487, 220);
+            pelota.setIcon(new ImageIcon("imagenes/pelota.png"));
+            pelota.setVisible(true);
+            panelJuego.add(pelota, 0);
             
             ventana.setVisible(true);
         }
